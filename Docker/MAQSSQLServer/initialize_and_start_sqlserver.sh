dc8ad7b7a0ece68f63efd7c891ac993e7164c24a
Brannen Hall <brannen@hallb.me>
2020-06-24 00:07:28 +0000

    Switch SQL server to use a Dockerfile

--- Docker/MAQSSQLServer/initialize_and_start_sqlserver.sh
+++ Docker/MAQSSQLServer/initialize_and_start_sqlserver.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-{ 
-    # Wait for SQL Server to start up
-    # Check if the server is ready
-    not_ready=1
-    while [ $not_ready != 0 ]
-    do
-        # Wait for the return code of the following statement to be zero
-        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -Q "SELECT TOP 1 message_id FROM sys.messages"
-        not_ready=$?
 
-        if [ $not_ready != 0 ]
-        then
-            echo "Could not contact sql server, will try again in 5 seconds."
-            sleep 5s
-        fi
-    done
+/opt/mssql/bin/sqlservr &
+sql_pid=$!
 
-    echo "Started initializing database"
-    # Set up the schema and stored procedures
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -i `dirname $0`/schema.sql
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -i `dirname $0`/stored_procedures.sql
-    # Use BCP to import test data
-    /opt/mssql-tools/bin/bcp MagenicAutomation.dbo.States in "`dirname $0`/SeedData/MagenicAutomation/States.bcp" \
-        -n -S localhost -U sa -P magenicMAQS2
-    /opt/mssql-tools/bin/bcp MagenicAutomation.dbo.Cities in "`dirname $0`/SeedData/MagenicAutomation/Cities.bcp" \
-        -n -S localhost -U sa -P magenicMAQS2
-    /opt/mssql-tools/bin/bcp MagenicAutomation.dbo.Datatype in "`dirname $0`/SeedData/MagenicAutomation/Datatype.bcp" \
-        -n -S localhost -U sa -P magenicMAQS2
-    echo "Finished initializing database"
-}&
+# Wait for SQL Server to start up
+# Check if the server is ready
+not_ready=1
+while [ $not_ready != 0 ]
+do
+    # Wait for the return code of the following statement to be zero
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -Q "SELECT TOP 1 message_id FROM sys.messages"
+    not_ready=$?
 
-# Start SQL server
-exec /opt/mssql/bin/sqlservr
+    if [ $not_ready != 0 ]
+    then
+        echo "Could not contact sql server, will try again in 5 seconds."
+        sleep 5s
+    fi
+done
+
+echo "Started initializing database"
+# Set up the schema and stored procedures
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -i `dirname $0`/schema.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P magenicMAQS2 -d master -i `dirname $0`/stored_procedures.sql
+# Use BCP to import test data
+/opt/mssql-tools/bin/bcp MagenicAutomation.dbo.States in "`dirname $0`/SeedData/MagenicAutomation/States.bcp" \
+    -n -S localhost -U sa -P magenicMAQS2
+/opt/mssql-tools/bin/bcp MagenicAutomation.dbo.Cities in "`dirname $0`/SeedData/MagenicAutomation/Cities.bcp" \
+    -n -S localhost -U sa -P magenicMAQS2
+/opt/mssql-tools/bin/bcp MagenicAutomation.dbo.Datatype in "`dirname $0`/SeedData/MagenicAutomation/Datatype.bcp" \
+    -n -S localhost -U sa -P magenicMAQS2
+echo "Finished initializing database"
+# Done bootstrapping
+# Kill SQL server
+kill -s 15 $sql_pid
